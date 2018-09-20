fb9b8f872e73b905b1aea780c707af8ebad138a0
vinicius <viniciussf@hotmail.com>
2018-09-20 17:14:48 -0300

    Refatoracao do login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,6 @@ import { AbstractComponent } from '../app.abstract.component';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LoginService } from './login.service';
 import { Http } from '@angular/http';
-import { FORMERR } from 'dns';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +14,7 @@ export class LoginComponent extends AbstractComponent {
   private login:Login;
   private erros:string;
   private loginService:LoginService ; 
-  private token:string;
+  
   constructor(http:Http,  route:ActivatedRoute, router:Router){
     super(route, router);
 this.loginService =new LoginService(http,route,router);
@@ -31,18 +30,10 @@ this.loginService =new LoginService(http,route,router);
  }
 
  autenticar(){
-  this.loginService.gerarToken(this.login).subscribe(
-    resp=> {
-      this.token=resp.data.token;
-      if(this.token!==undefined){
-        this.redirect({path:['/pessoa']});
-      } else {
-        this.erros='Falha na geração de token para o usuário: '+this.login.email;
-      }
-   },
-   error=>{
-    this.erros='Usuário/Senha inválidos!';
-   });
+  this.loginService.gerarToken(this.login, 
+      (resp)=> this.redirect({path:['/pessoa']}), 
+      (error)=>this.erros='Usuário/Senha inválidos!'
+    );
  }
 
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -8,12 +8,27 @@ export class LoginService extends GenericService{
     private static token:string;
     private static readonly AUTENTICATION_HOST:string = '//localhost:8081';
       erros:string;
+      private token:string;
+  private roles:string[];
   constructor( http:Http,  route:ActivatedRoute,router:Router ){
        super(http,router,route);
 }
-    gerarToken(login:Login, ok?:any, fail?:any):Observable<any>{
-        return this.httpPost('/auth', {"email":login.email, "senha":login.senha}, 
-                    LoginService.AUTENTICATION_HOST);
+    gerarToken(login:Login, ok?:any, fail?:any){
+        this.httpPost('/auth', {"email":login.email, "senha":login.senha}, LoginService.AUTENTICATION_HOST)
+                    .subscribe(
+                        resp=>{
+                            this.token=resp.data.token;
+                            this.roles = resp.data.roles;
+                            if(ok!==undefined){
+                                ok(resp);
+                            }
+                        }, 
+                        error=>{
+                            if(fail!==undefined){
+                                fail(error);    
+                            }
+                        }
+                    );
                        
     }
 
